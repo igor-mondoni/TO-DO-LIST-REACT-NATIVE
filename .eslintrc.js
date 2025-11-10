7d907353b4c21e4df42224eaf71b79b6a2268c9c
Igor Mondoni <igor.hm17@hotmail.com>
2025-11-10 19:51:33 -0300

    Configurando eslint, prettier e package json

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'react',
+    'react-hooks',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
